4920653e1d7342fcbc99646700b8415a64cbeb0e
Kaan Özer <kaanozer35@gmail.com>
2023-02-25 14:55:08 +0300

    updates

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let addBtn = document.querySelector(".plus-icon-anchor");
+let pomodoroList = document.querySelector("#pomodoroList");
+
+addBtn.addEventListener("click", addPomodoro);
+
+function addPomodoro() {
+  let pomodoroLi = document.createElement("li");
+  pomodoroLi.innerHTML = "test";
+
+  pomodoroList.append(pomodoroLi);
+}
